97aa7b43992e63e0dc636ebb279f6d16941e07f3
Ramone Robertson <ramonerobertson10@gmail.com>
2025-02-18 18:34:58 +0900

    finished longest substring challenge

--- LongestSubString/src/main/java/Main.java
+++ LongestSubString/src/main/java/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String [] args){
+        int test1 = Solution.lengthOfLongestSubString("pwwkew");
+        System.out.println(test1);
+    }
 }
--- LongestSubString/src/main/java/Solution.java
+++ LongestSubString/src/main/java/Solution.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+
+    public static int lengthOfLongestSubString(String s){
+        // Map to store already seen characters
+        Map <Character, Integer> seenChars = new HashMap<>();
+
+        // Set our pointers for the window
+        int left = 0;
+        int right = 0;
+
+        // Keep track of the longest substring found
+        int maxLength = 0;
+
+        // While our right side of the window is less than the length of the string
+        while(right < s.length()) {
+            char charAtRight = s.charAt(right);
+
+            // add the char and freq to the hash: (character, frequency)
+            // We add 1 because we have now seen the character at least once
+            seenChars.put(charAtRight, seenChars.getOrDefault(charAtRight, 0) + 1);
+
+            // While the frequency of the character is greater than 1 (means there are duplicates in our window) so we need to shrink the window
+            while(seenChars.get(charAtRight) > 1){
+                // Set the left side char
+                char charAtLeft = s.charAt(left);
+
+                // Now we need to reduce the frequency back down to one to remove dupes from the window
+                seenChars.put(charAtLeft, seenChars.get(charAtLeft) - 1);
+
+                // Shrink the window by moving left 1 step
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+        return maxLength;
+    }
+
 }
